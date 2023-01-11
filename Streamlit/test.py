4f1b43aeab48763d6c86cb6cb5c25c410e88baab
MugicaLaurendi <laurendi.mugica@gmail.com>
2023-01-11 14:36:15 +0100

    add streamlit

---
+++ Streamlit/test.py
@@ -0,0 +1,11 @@
+import requests
+
+#response = requests.get("https://swapi.dev/api/people/").json()
+
+#print(response)
+
+#print(response["results"][0]["name"])
+
+bob = "bienvenu ma saucisse "
+a = bob.find("lol")
+print(a)
