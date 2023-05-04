2f01e9d5de0ef557e6634d88d6d90c9130134091
MugicaLaurendi <laurendi.mugica@gmail.com>
2023-05-04 11:08:27 +0200

    test

--- Streamlit/traductor_streamlit/streamlit_app.py
+++ Streamlit/traductor_streamlit/streamlit_app.py
@@ -17,4 +17,4 @@ result_count = []
 
 data = st.file_uploader("Upload a audio file to translate", type=["mp3"])
 
-response = requests.get(f"https://swapi.dev/api/{option}/").json()
+#response = requests.get(f"https://swapi.dev/api/{option}/").json()
