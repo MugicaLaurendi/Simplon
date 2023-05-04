83810e8002921401749c9e6eb5686efb2a577fee
MugicaLaurendi <laurendi.mugica@gmail.com>
2023-05-04 10:26:56 +0200

    add traducor_streamlit

---
+++ Streamlit/traductor_streamlit/streamlit_app.py
@@ -0,0 +1,63 @@
+from collections import namedtuple
+import altair as alt
+import math
+import pandas as pd
+import streamlit as st
+import requests
+"""
+# Bienvenue sur mon API starwars
+
+Evidemment cette API est un lien direct vers SWAPI :heart:
+Ce streamlit est un exercice
+
+
+"""
+
+result_count = []
+
+option = st.selectbox(
+    'Choose category :',
+    ('','people', 'starships', 'planets'))
+
+response = requests.get(f"https://swapi.dev/api/{option}/").json()
+
+
+
+if option != '' :
+
+    ###
+
+    searched = st.text_input(f"Search in {option}")
+
+    st.write(f'{len(result_count)} results')
+
+
+    for i in response["results"] :
+
+        if searched != '' :
+
+            name = i["name"].lower()
+            is_in = name.find(searched)
+
+            if is_in >= 0 :
+                result_count.append(i["name"])
+                st.write("---------------")
+
+                for y in i :
+
+                    if y == "name" :
+                        st.write(f"### {i[y]}")
+
+                    else :
+                        st.write(f"{y} : {i[y]}")
+        else :
+            result_count.append(i["name"])
+            st.write("---------------")
+
+            for y in i :
+
+                    if y == "name" :
+                        st.write(f"### {i[y]}")
+
+                    else :
+                        st.write(f"{y} : {i[y]}")
